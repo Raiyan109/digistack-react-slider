8ca9429d4c251763fe47a5e3c0c50147147e0e31
Raiyan109 <kraiyan109@gmail.com>
2023-12-05 22:18:13 +0600

    isActive class done, now button

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '100px' }}>
       <Swiper1 />
       <Swiper2 />
       {/* <SwipPractice /> */}
--- src/components/Swiper2.jsx
+++ src/components/Swiper2.jsx
@@ -2,11 +2,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
 import Swiper2Button from './Swiper2Button';
 
-// import styles from '../swiper2.module.css'
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import '../swiper2.css'
+
+import img1 from '../assets/blog-1.png'
 
 const Swiper2 = () => {
     // const swiperRef = useRef(null);
@@ -67,19 +69,31 @@ const Swiper2 = () => {
                 modules={[EffectCoverflow, Pagination, Navigation]}
                 className=""
             >
-                <div className='swiper-content-wrapper'>
-                    <div className='swiper-header-wrapper'>
-                        <h1 style={{ fontSize: '40px', textTransform: 'uppercase', color: 'black', fontWeight: 'normal' }}>Our <span style={{ fontSize: '40px', textTransform: 'uppercase', color: 'black', fontWeight: 'bolder' }}>Works of Art:</span></h1>
+                <div className='swiper2-content-wrapper'>
+                    <div className='swiper2-header-wrapper'>
+                        <h1 style={{ fontSize: '40px', textTransform: 'uppercase', color: 'black', fontWeight: 'normal', lineHeight: '1px' }}>Our <span style={{ fontSize: '40px', textTransform: 'uppercase', color: 'black', fontWeight: 'bolder' }}>Works of Art:</span></h1>
                         <h1 style={{ fontSize: '40px', textTransform: 'uppercase', color: 'black' }}>Unfolding Design stories</h1>
-                        <p>Solli lobortis risus eget iaculis tincidunt placerat neque nisi. Aliquam at nibh mauris facilisis in sed aliquet. Feugiat massa mollis commodo sed sagittis vestibulum libero diam id.</p>
+                        <p className='swiper2-paragraph'>Solli lobortis risus eget iaculis tincidunt placerat neque nisi. Aliquam at nibh mauris facilisis in sed aliquet. Feugiat massa mollis commodo sed sagittis vestibulum libero diam id.</p>
                     </div>
                 </div>
                 <SwiperSlide>
-                    <img className='swiper-img' src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                    <p className='swiper-p'>Residential interior design</p>
+                    {({ isActive }) => (
+                        <div>
+                            {isActive ?
+                                <div>
+                                    <img className='swiper-img ' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                    <p className='swiper-p'>Residential interior design</p>
+                                </div>
+                                :
+                                <div className=''>
+                                    <img className=' in-active' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+
+                                </div>
+                            }
+                        </div>
+                    )}
                 </SwiperSlide>
                 <SwiperSlide
-                // className={styles.swiperSlide2}
                 >
                     {({ isActive }) => (
                         <div>
@@ -101,17 +115,72 @@ const Swiper2 = () => {
                     <Swiper2Button />
                 </div>
                 <SwiperSlide>
-                    <img className='swiper-img' src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                    <p className='swiper-p'>Royal bedroom</p>
+                    {({ isActive }) => (
+                        <div>
+                            {isActive ?
+                                <div>
+                                    <img className='swiper-img ' src={img1} />
+                                    <p className='swiper-p'>Residential interior design</p>
+                                </div>
+                                :
+                                <div className=''>
+                                    <img className=' in-active' src={img1} />
+
+                                </div>
+                            }
+                        </div>
+                    )}
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='swiper-img' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                    {({ isActive }) => (
+                        <div>
+                            {isActive ?
+                                <div>
+                                    <img className='swiper-img ' src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                                    <p className='swiper-p'>Residential interior design</p>
+                                </div>
+                                :
+                                <div className=''>
+                                    <img className=' in-active' src="https://swiperjs.com/demos/images/nature-3.jpg" />
+
+                                </div>
+                            }
+                        </div>
+                    )}
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='swiper-img' src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                    {({ isActive }) => (
+                        <div>
+                            {isActive ?
+                                <div>
+                                    <img className='swiper-img ' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                                    <p className='swiper-p'>Residential interior design</p>
+                                </div>
+                                :
+                                <div className=''>
+                                    <img className=' in-active' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+
+                                </div>
+                            }
+                        </div>
+                    )}
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='swiper-img' src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                    {({ isActive }) => (
+                        <div>
+                            {isActive ?
+                                <div>
+                                    <img className='swiper-img ' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                                    <p className='swiper-p'>Residential interior design</p>
+                                </div>
+                                :
+                                <div className=''>
+                                    <img className=' in-active' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+
+                                </div>
+                            }
+                        </div>
+                    )}
                 </SwiperSlide>
 
             </Swiper>
